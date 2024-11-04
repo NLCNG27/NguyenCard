ffa2ecb572f82398da2c7291cc43020c74e0a3a1
Nguyen Lam <nguyen@cngsoftware.com>
2024-11-03 22:28:59 -0600

    Initial Commit

---
+++ NguyenCard/NguyenCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  NguyenCardApp.swift
+//  NguyenCard
+//
+//  Created by Nguyen Lam on 11/3/24.
+//
+
+import SwiftUI
+
+@main
+struct NguyenCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
